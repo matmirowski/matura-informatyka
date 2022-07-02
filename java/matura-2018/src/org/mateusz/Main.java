6cfca24f1c005ca83b8e3504b13c018227486782
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2022-07-03 01:06:25 +0200

    2018 - Finish task 4.3

--- java/matura-2018/src/org/mateusz/Main.java
+++ java/matura-2018/src/org/mateusz/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Task4_1.run();
         Task4_2.run();
+        Task4_3.run();
     }
 }
--- java/matura-2018/src/org/mateusz/Task4_3.java
+++ java/matura-2018/src/org/mateusz/Task4_3.java
@@ -1,4 +1,36 @@
 package org.mateusz;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Task4_3 {
+    public static void run() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("sygnaly.txt"));
+        String line;
+        ArrayList<String> results = new ArrayList<>();
+
+        while ((line = reader.readLine()) != null) {
+            int highest_ascii = 0;
+            int lowest_ascii = 100;
+            char[] chars = line.toCharArray();
+
+            for (char letter: chars) {
+                int ascii = (int) letter;
+                if (ascii > highest_ascii)
+                    highest_ascii = ascii;
+                if (ascii < lowest_ascii)
+                    lowest_ascii = ascii;
+            }
+
+            if ((highest_ascii - lowest_ascii) <= 10)
+                results.add(line);
+        }
+
+        System.out.println("Task 4.3");
+        for (String result : results) {
+            System.out.println(result);
+        }
+    }
 }
