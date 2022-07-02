c910cd6e6540ab6d681c7f118cdf972cafecfa74
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2022-07-02 15:42:40 +0200

    Finish task 6.2

--- java/matura-2017/src/org/mateusz/Main.java
+++ java/matura-2017/src/org/mateusz/Main.java
@@ -6,5 +6,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Task6_1.run();
+        Task6_2.run();
     }
 }
--- java/matura-2017/src/org/mateusz/Task6_2.java
+++ java/matura-2017/src/org/mateusz/Task6_2.java
@@ -1,4 +1,37 @@
 package org.mateusz;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Task6_2 {
+    public static void run() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("przyklad.txt"));
+        String line;
+        int invalidLines = 0;
+
+        while ((line = reader.readLine()) != null) {
+            String[] numbers = line.split(" ");
+
+            if (!isPalindrome(numbers)) {
+                invalidLines++;
+            }
+
+        }
+        System.out.println("Task 6.2");
+        System.out.println("Invalid lines: " + invalidLines);
+        System.out.println();
+    }
+
+    private static boolean isPalindrome(String[] numbers) {
+        for (int i=0; i<320; i++) {
+            if (!numbers[i].equals(numbers[319-i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
 }
