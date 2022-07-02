dab793a92dd7d5a6e7dd6665737baf2966c1b468
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2022-07-02 14:48:03 +0200

    Finish task 6.1

--- java/matura-2017/src/org/mateusz/Task6_1.java
+++ java/matura-2017/src/org/mateusz/Task6_1.java
@@ -1,8 +1,34 @@
 package org.mateusz;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Task6_1 {
 
-    public static void run() {
+    public static void run() throws IOException {
 
+        BufferedReader reader = new BufferedReader(new FileReader("dane.txt"));
+        String line;
+        String[] numbers;
+        int brightest = 0;
+        int darkest = 255;
+
+        while ((line = reader.readLine()) != null) {
+            numbers = line.split(" ");
+
+            for(int i=0; i<320; i++) {
+                int num = Integer.parseInt(numbers[i]);
+                if (num > brightest) {
+                    brightest = num;
+                }
+                if (num < darkest) {
+                    darkest = num;
+                }
+            }
+        }
+
+        System.out.println("Brightest: " + brightest);
+        System.out.println("Darkest " + darkest);
+        reader.close();
     }
 }
