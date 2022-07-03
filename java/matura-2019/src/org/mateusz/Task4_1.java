5f00bfcee918b4052e6d6cb4cea3cbee45f1e167
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2022-07-03 12:34:09 +0200

    2019 - Finish task 4.1

--- java/matura-2019/src/org/mateusz/Task4_1.java
+++ java/matura-2019/src/org/mateusz/Task4_1.java
@@ -1,4 +1,32 @@
 package org.mateusz;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Task4_1 {
+    public static void run() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("liczby.txt"));
+        String line;
+        int result = 0;
+
+        while ((line = reader.readLine()) != null) {
+            int number = Integer.parseInt(line.strip());
+            if (isPowerOf3(number))
+                result++;
+        }
+
+        System.out.println("Task 4.1");
+        System.out.println("Result: " + result);
+        System.out.println();
+    }
+
+    private static boolean isPowerOf3(int number) {
+        for (int i=1; i<100001; i=i*3) {
+            if (number == i)
+                return true;
+        }
+        return false;
+    }
 }
