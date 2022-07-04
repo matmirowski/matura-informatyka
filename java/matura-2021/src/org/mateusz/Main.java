65a28c2389fbf813a66830ddf84276ac084a1d09
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2022-07-04 22:10:34 +0200

    2021 - Finish task 4.3

--- java/matura-2021/src/org/mateusz/Main.java
+++ java/matura-2021/src/org/mateusz/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Task4_1and4_4.run();
         Task4_2.run();
+        Task4_3.run();
     }
 }
--- java/matura-2021/src/org/mateusz/Task4_3.java
+++ java/matura-2021/src/org/mateusz/Task4_3.java
@@ -1,4 +1,48 @@
 package org.mateusz;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task4_3 {
+    private static int highestValue = 0;
+    private static char highestLetter = ' ';
+    public static void run() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("instrukcje.txt"));
+        String line;
+        String result = "";
+        HashMap<Character, Integer> letterToCount = generateMap();
+
+        while ((line = reader.readLine()) != null) {
+            String[] order = line.strip().split(" ");
+
+            if (order[0].equals("DOPISZ")) {
+                char letter = order[1].charAt(0);
+                int counter = letterToCount.get(letter);
+                counter++;
+                letterToCount.put(letter, counter);
+            }
+        }
+
+        letterToCount.forEach((letter, value) -> {
+            if (value > highestValue) {
+                highestValue = value;
+                highestLetter = letter;
+            }
+        });
+
+        System.out.println("Task 4.3");
+        System.out.println(highestLetter + " " + highestValue);
+    }
+
+    public static HashMap<Character,Integer> generateMap() {
+        HashMap<Character, Integer> letterToCount = new HashMap<>();
+        for (int i=65; i<=90; i++) {
+            letterToCount.put((char) i, 0);
+        }
+        return letterToCount;
+    }
 }
