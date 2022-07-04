c24c6445db98fc059bff321f6899ed0b7b72d137
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2022-07-04 12:13:28 +0200

    2020 - Created project structure

---
+++ java/matura-2020/src/org/mateusz/Main.java
@@ -0,0 +1,9 @@
+package org.mateusz;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Task4_1.run();
+    }
+}
---
+++ java/matura-2020/src/org/mateusz/Task4_1.java
@@ -0,0 +1,12 @@
+package org.mateusz;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Task4_1 {
+    public static void run() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("przyklad.txt"));
+    }
+}
