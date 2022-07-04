5b9b0901d7dff0831e4dd669062ec4d1edd55de4
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2022-07-04 14:23:44 +0200

    2020 - Task 4.3 fix

--- java/matura-2020/src/org/mateusz/Task4_3.java
+++ java/matura-2020/src/org/mateusz/Task4_3.java
@@ -10,39 +10,34 @@ public class Task4_3{
         String line;
         String[] numAndWord;
         boolean firstWord = true;
-        String[] lowestWord = new String[2];
+        String lowestWord = "";
+
         while ((line = reader.readLine()) != null) {
             numAndWord = line.strip().split(" ");
             int number = Integer.parseInt(numAndWord[0]);
             String word = numAndWord[1];
-
 
             if (number != word.length()) // if number isnt equal to length of world, we skip that case
                 continue;
 
             if (firstWord) { // if it's first word, we set it to be lowestWord
                 firstWord = false;
-                lowestWord[0] = numAndWord[0];
-                lowestWord[1] = numAndWord[1];
+                lowestWord = word;
                 continue;
             }
 
-            if (number < Integer.parseInt(lowestWord[0])) { // if its number is lower than currently lowest number
-                lowestWord[0] = numAndWord[0];
-                lowestWord[1] = numAndWord[1];
-            }
+            if (number < lowestWord.length()) // if its number is lower than currently lowest number
+                lowestWord = word;
 
-            else if (number == Integer.parseInt(lowestWord[0])) { // if its number is equal to corrently lowest number
-                if (word.compareTo(lowestWord[1]) < 0) { // we have to check if word is lower alphabetically
-                    lowestWord[0] = numAndWord[0];
-                    lowestWord[1] = numAndWord[1];
-                }
-            }
+            else if (number == lowestWord.length()) // if its number is equal to corrently lowest number
+                if (word.compareTo(lowestWord) < 0) // we have to check if word is lower alphabetically
+                    lowestWord = word;
+
         }
 
         System.out.println();
         System.out.println("Task 4.3");
-        System.out.println(lowestWord[0] + " " + lowestWord[1]);
+        System.out.println(lowestWord.length() + " " + lowestWord);
     }
 
 }
